0a959522d207015f6041cb9cb51921e4f1c38c35
Lawnless <69431018+Lawnless@users.noreply.github.com>
2021-07-19 13:37:27 +0300

    Create texts.js

---
+++ settings/texts.js
@@ -0,0 +1,6 @@
+texts = '[\
+"Kullandıktan sonra silmeyi unutma yoksa herkes okuyabilir, Tek yapman gereken asanla yine tıklayıp, \'Muziplik tamamlandı!\' demek. Üstü hemen bomboş olacak.",\
+"Gir bakalım, yabancı, ama dikkat et, sakın kendini koyverip de hırsa kapılmayasın, alın teri dökmeden köşe dönme hevesi canına okur sonra, bak bizden söylemesi, senin olmayan bir şey yürüteceksen unut aklını başına al, sonra da kendini tut, hırsızlığa kalkarsan, bir daha düşün yine, başka şeyler bulursun çil altınlar yerine.",\
+"Ruh Emici\'ler bu dünyadaki en berbat yaratıklardandır. En karanlık, en pis yerlerde barınırlar, çürümeden ve umutsuzluktan zevk alırlar, etraflarındaki huzuru, umudu ve mutluluğu kuruturlar. Bir Ruh Emici’ye fazla yaklaştığında bütün iyi duyguların, bütün mutlu anıların emilip alınır senden. Bir Ruh Emici, eğer başarabilir de seninle uzun süre beslenirse, sonunda seni de kendi gibi bir şey haline getirir(ruhsuz ve kötücül). Elinde hayatındaki en kötü deneyimlerin haricinde hiçbir şey kalmaz."\
+]\
+';
